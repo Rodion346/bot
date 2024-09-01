bfa49a50d33d13a9d9805ce0bb773ea058a33b7e
Rodion346 <rodionkireev44@gmail.com>
2024-09-01 17:23:10 +0300

    add referals programs

--- bot/routes/top_balance.py
+++ bot/routes/top_balance.py
@@ -13,8 +13,8 @@ router_balance = Router()
 
 @router_balance.callback_query(lambda p: 'sum_' in p.data)
 async def process_callback_button(call: CallbackQuery):
-    credit = call.data.split('_')[1]
-    await call.message.answer(f"{credit[2]}")
+    credit = call.data.split('_')
+    await call.message.answer(f"{credit}")
     credit = credit[2].split(" ", 1)[0].strip()
     payload = {"id": f"{call.from_user.id}", "new_balance": int(credit), "type_balance": 1}
     requests.post(f"{BASE_URL_API}/api/v1/user/{call.from_user.id}", json=payload).json()
